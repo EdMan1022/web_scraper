9b29c2c9bfb5a5ae05f798b60363e6c54a1a3dc9
EdMan1022 <EdMan1022@gmail.com>
2018-02-08 15:06:32 -0500

    Added method to select station and time range to `WebCrawler`

--- web_scraper/classes/web_crawler.py
+++ web_scraper/classes/web_crawler.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import sys
 from web_scraper.extensions import db
+import time
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class LoginIDs(object):
@@ -27,6 +29,19 @@ class WebCrawler(object):
     login_ids = LoginIDs(db.app.config['MEDIA_MONITORS_USERNAME'],
                          db.app.config['MEDIA_MONITORS_PASSWORD'])
     login_button_id = "btnLogin"
+
+    audience_reaction_url = "https://www2.mediamonitors.com/v2/app/Reports/AudienceReaction"
+    media_selection_id = "txtMediaSelection"
+
+    station_window_id = "ui-id-1"
+    station_match_text = "(//*[contains(text(), '{}')] | //*[@value='{}'])"
+    station_window_button_class = "ui-dialog-buttonset"
+
+    input_pause_time = .5
+    buttonset_text = "OKCancel"
+    button_tag = "button"
+    ok_text = "OK"
+    time_range_id = "ctrlDateRange"
 
     def __init__(self):
         os.environ[self.moz_env_key] = self.moz_env_value
@@ -58,4 +73,71 @@ class WebCrawler(object):
         self.send_input_by_id(self.login_ids.password_key, self.login_ids.password)
         submit = self.driver.find_element_by_id(self.login_button_id)
         submit.click()
+
+    def select_station(self, station_name: str):
+        """
+        Selects the correct station
+        :param station_name:
+        :return:
+        """
+        station_drop_down = self.driver.find_element_by_id(self.media_selection_id)
+        station_drop_down.click()
+        time.sleep(self.input_pause_time)
+        station_window = self.driver.find_element_by_id(self.station_window_id)
+        element_list = station_window.find_elements_by_xpath(self.station_match_text.format(station_name,
+                                                                                            station_name))
+        reduced_element_list = [element for element in element_list if element.text == station_name]
+        station_element = reduced_element_list[0]
+
+        station_element.click()
+
+        try:
+            buttonset_list = self.driver.find_elements_by_class_name(self.station_window_button_class)
+            buttonset = [element for element in buttonset_list if element.text == self.buttonset_text][0]
+
+            buttons = buttonset.find_elements_by_tag_name(self.button_tag)
+            ok_button = [element for element in buttons if element.text == self.ok_text][0]
+
+            ok_button.click()
+        except IndexError:
+            pass
+
+    def button_click(self, button):
+        hover = ActionChains(self.driver).move_to_element(button)
+        hover.click().perform()
+
+    def select_date(self, date):
+        pass
+
+
+    def select_time_range(self, time_start, time_end):
+        time_range_element = self.driver.find_element_by_id(self.time_range_id)
+        time_range_buttons = self.driver.find_elements_by_tag_name(self.button_tag)
+        start_button = time_range_buttons[0]
+        end_button = time_range_buttons[1]
+        self.button_click(start_button)
+        self.select_date(time_start)
+
+        self.button_click(end_button)
+        self.select_date(time_end)
+
+
+
+
+
+
+
+
+
+    def update_station_trends(self, station_name: str):
+
+        self.driver.get(self.audience_reaction_url)
+        self.select_station(station_name)
+
+
+
+
+
+
+
 
